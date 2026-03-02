77100e54eb22765ea6ac4b9d5ce1480a6aa5e16d
sehamalkubara-pixel <sehamalkubara@gmail.com>
2026-03-03 01:56:50 +0300

    Add disk space information output to check-disk.ps1

--- check-disk.ps1
+++ check-disk.ps1
@@ -1,1 +1,4 @@
-Get-PSDrive -PSProvider FileSystem
+```powershell id="egnbk0"
+# Script: check available disk space
+Write-Output "=== Disk Space Information ==="
+Get-PSDrive -PSProvider FileSystem | Format-Table Name,Free,Used,Root
